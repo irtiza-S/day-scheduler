6616dd112b0f629b2f71ed8c2361fc9625af0a73
destro <irtizasafdar@hotmail.co.uk>
2024-03-12 10:15:58 +0000

    implemented current state of timeblock - timeState(), currentDate() - also complete

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,57 @@
+
 $(document).ready(function() {
 
     // Extend Day.js with the relativeTime plugin
     dayjs.extend(window.dayjs_plugin_relativeTime);
-    var today = dayjs().format('dddd, MMMM DD');
-    $('#currentDay').text(today);
 
-    // thought process: 
-    //user enters into the text area
-    //press the save btn - leading to whatever they've entered being saved to localstorage
-    var buttons = $('.btn')
-    console.log(buttons.children)
+    // display current date - Day, Month, Date
+    function currentDate() {
+        var today = dayjs().format('dddd, MMMM DD');
+        $('#currentDay').text(today); 
+    }
+
+
+
+    function timeState() {
+        const currentHr = dayjs().format('H')
+        console.log(currentHr)
+        const timeblock = $('.time-block')
+    
+        timeblock.each(function () {
+            var timeblockId = $(this).attr('id') 
+            var textareaSibling = $(this).siblings('.time-slot').find('textarea');
+            console.log(timeblockId)
+            var blockHours = parseInt(timeblockId)
+
+            if (currentHr == blockHours) {
+                $(this).removeClass('past')
+                $(this).removeClass('future')
+                $(this).addClass("present");
+                textareaSibling.removeClass('past')
+                textareaSibling.removeClass('future')
+                textareaSibling.addClass("present");
+                textareaSibling.prop('disabled', false)
+            }
+            else if (currentHr > blockHours) {
+                $(this).removeClass('present')
+                $(this).removeClass('future')
+                $(this).addClass('past')
+                textareaSibling.removeClass('present')
+                textareaSibling.removeClass('future')
+                textareaSibling.addClass('past')
+                textareaSibling.prop('disabled', true)
+            } else {
+                $(this).removeClass('present')
+                $(this).removeClass('past')
+                $(this).addClass('future')
+                textareaSibling.removeClass('present')
+                textareaSibling.removeClass('past')
+                textareaSibling.addClass('future')
+                textareaSibling.prop('disabled', false)
+            }
+        }) 
+    }
+    
+    currentDate()
+    timeState()
   });
